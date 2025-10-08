613514418006242b375281cfdf25b5b49dc93df1
Eryk Kryszewski <eric9972@o2.pl>
2025-10-08 20:17:59 +0200

    before wcag

--- js/src/sections/header.js
+++ js/src/sections/header.js
@@ -1,17 +1,26 @@
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= 50) {
-    document.querySelector('.header').classList.add('header--fixed');
-    document.querySelector('.nav').classList.add('nav--fixed');
-    document.querySelector('.nav__logo').classList.add('nav__logo--fixed');
-    document.querySelector('.nav__menu').classList.add('nav__menu--fixed');
-    document.querySelector('.nav__button').classList.add('nav__button--fixed');
-    document.querySelector('.nav__hamburger').classList.add('nav__hamburger--fixed');
-  } else {
-    document.querySelector('.header').classList.remove('header--fixed');
-    document.querySelector('.nav').classList.remove('nav--fixed');
-    document.querySelector('.nav__logo').classList.remove('nav__logo--fixed');
-    document.querySelector('.nav__menu').classList.remove('nav__menu--fixed');
-    document.querySelector('.nav__button').classList.remove('nav__button--fixed');
-    document.querySelector('.nav__hamburger').classList.remove('nav__hamburger--fixed');
-  }
+window.addEventListener("scroll", function () {
+    const headerElement = document.querySelector(".header");
+    const scrollNow = window.scrollY;
+
+    if (scrollNow >= 60) {
+        headerElement.classList.add("header--fixed");
+        headerElement.querySelector(".nav").classList.add("nav--fixed");
+        headerElement.querySelector(".nav__logo").classList.add("nav__logo--fixed");
+        headerElement.querySelector(".nav__menu").classList.add("nav__menu--fixed");
+        headerElement.querySelector(".nav__button").classList.add("nav__button--fixed");
+        headerElement.querySelector(".nav__hamburger").classList.add("nav__hamburger--fixed");
+    } else if (scrollNow < 30) {
+        headerElement.classList.remove("header--fixed");
+        headerElement.querySelector(".nav").classList.remove("nav--fixed");
+        headerElement.querySelector(".nav__logo").classList.remove("nav__logo--fixed");
+        headerElement.querySelector(".nav__menu").classList.remove("nav__menu--fixed");
+        headerElement.querySelector(".nav__button").classList.remove("nav__button--fixed");
+        headerElement.querySelector(".nav__hamburger").classList.remove("nav__hamburger--fixed");
+    }
+
+    if (scrollNow > 5) {
+        headerElement.classList.add("header--background");
+    } else {
+        headerElement.classList.remove("header--background");
+    }
 });
